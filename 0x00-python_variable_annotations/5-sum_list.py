7374f2a64f2d573c279b2fd0e191ecf99143c3cb
starlingvibes <starlingroot@protonmail.com>
2023-01-19 01:51:26 +0100

    using an earlier syntax

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,8 +3,9 @@
 """
 Type-annotating complex types
 """
+from typing import List
 
 
-def sum_list(input_list: list[float, ...]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Returns the sum of a list of floats"""
     return sum(input_list)
