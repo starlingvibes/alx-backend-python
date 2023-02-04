9177de38694d74431a48f9adcdeef2103954abf6
starlingvibes <starlingroot@protonmail.com>
2023-02-04 01:25:25 +0100

    float type annotation in python

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Python type annotation with floats
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the input float
+    """
+    return math.floor(n)
