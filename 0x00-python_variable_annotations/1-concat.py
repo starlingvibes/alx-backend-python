d035056258994ad31a2851111807e8e6b8b06822
starlingvibes <starlingroot@protonmail.com>
2023-02-04 01:19:10 +0100

    type annotations with strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Basic type annotations - Concat
+"""
+
+
+def concat(str1: string, str2: string) -> string:
+    """
+    Returns a concatenated string by joining str1 and str2
+    """
+    return "{}{}".format(str1, str2)
