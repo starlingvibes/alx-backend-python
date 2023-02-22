65af11fa9cc9e0587fc5fbfe02fd895a665ecad2
starlingvibes <starlingroot@protonmail.com>
2023-02-22 21:27:26 +0100

    added async generator function

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+import random
+import asyncio
+
+"""
+Task 0's module
+"""
+
+
+async def async_generator():
+    """async_generator function"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
