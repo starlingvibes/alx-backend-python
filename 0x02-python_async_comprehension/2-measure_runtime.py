ff942d622d78bdea011aadd63420cd43a8cf2fbe
starlingvibes <starlingroot@protonmail.com>
2023-02-23 22:26:24 +0100

    runtime for parallel comprehensions

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Run time for four parallel comprehensions
+"""
+import time
+import asyncio
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Run time for four parallel comprehensions"""
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return (time.time() - start_time)
